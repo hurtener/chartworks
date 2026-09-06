11f3e4cc1b588eb3472bcf4121787328f91e7fdf
Santi Benvenuto <benvenuto.santiago@hotmail.com>
2026-09-06 03:00:26 -0300

    feat(jobs): bind request manifests to stable canonical digests

---
+++ internal/jobs/request_manifest.go
@@ -0,0 +1,14 @@
+package jobs
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+)
+
+func requestDigest(value any) string {
+	encoded,err:=json.Marshal(value)
+	if err!=nil { return "" }
+	sum:=sha256.Sum256(encoded)
+	return hex.EncodeToString(sum[:])
+}
